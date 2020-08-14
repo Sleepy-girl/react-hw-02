5a0f04a9aa6d94be29cdf2a413b486da7b023cd7
nyuta <pillowforneedles@gmail.com>
2020-08-15 00:43:36 +0300

    Updates

---
+++ precache-manifest.d0942a65497349f7f61aebb040e595fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6d430c623d2548dc0b5c5727a9b2d26",
+    "url": "/react-hw-02/index.html"
+  },
+  {
+    "revision": "b27474b57d285132975a",
+    "url": "/react-hw-02/static/css/main.07a440ca.chunk.css"
+  },
+  {
+    "revision": "fbd4cfa9634f8ca90cf3",
+    "url": "/react-hw-02/static/js/2.3f9d8440.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-02/static/js/2.3f9d8440.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b27474b57d285132975a",
+    "url": "/react-hw-02/static/js/main.5480e113.chunk.js"
+  },
+  {
+    "revision": "10a12c2f38deb433bd14",
+    "url": "/react-hw-02/static/js/runtime-main.c7da64a4.js"
+  }
+]);
